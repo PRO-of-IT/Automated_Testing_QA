6cc6f439d72e29fd7e31334ff0a8a606eef3ace7
PRO-of-IT <pro.of.attila@gmail.com>
2022-07-21 11:18:48 +0300

    commit 1.0.1 ( generator data )

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -1,5 +1,5 @@
 import time
-
+from generator.generator import generated_person
 from locators.elements_page_locators import TextBoxPageLocators
 from pages.base_page import BasePage
 
@@ -8,9 +8,22 @@ class TextBoxPage(BasePage):
     locators = TextBoxPageLocators()
 
     def fill_all_fields(self):
-        self.element_is_visible(self.locators.FULL_NAME).send_keys('Alex')
-        self.element_is_visible(self.locators.EMAIL).send_keys('prof@mail.ru')
-        self.element_is_visible(self.locators.CURRENT_ADDRESS).send_keys('street 17')
-        self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_keys('Kharkiv')
+        person_info = next(generated_person())
+        full_name = person_info.full_name
+        email = person_info.email
+        current_address = person_info.current_address
+        permanent_address = person_info.permanent_address
+        self.element_is_visible(self.locators.FULL_NAME).send_keys(full_name)
+        self.element_is_visible(self.locators.EMAIL).send_keys(email)
+        self.element_is_visible(self.locators.CURRENT_ADDRESS).send_keys(current_address)
+        self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_keys(permanent_address)
         self.element_is_visible(self.locators.SUBMIT).click()
-        time.sleep(10)
+        time.sleep(15)
+        return full_name, email, current_address, permanent_address
+
+    def check_filled_form(self):
+        full_name = self.element_is_present(self.locators.CREATED_FULL_NAME).text.split(':')[1]
+        email = self.element_is_present(self.locators.CREATED_EMAIL).text.split(':')[1]
+        current_address = self.element_is_present(self.locators.CREATED_CURRENT_ADDRESS).text.split(':')[1]
+        permanent_address = self.element_is_present(self.locators.CREATED_PERMANENT_ADDRESS).text.split(':')[1]
+        return full_name, email, current_address, permanent_address
